30b946219e3efc5e40060d622c459d9fe64ca6b8
olesosa <oleosad@gmail.com>
2024-03-19 13:50:07 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainPageComponent} from "./pages/main-page/main-page.component";
+import {AdminPageComponent} from "./pages/admin-page/admin-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: MainPageComponent
+  },
+  {
+    path:'admin-page',
+    component: AdminPageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ButtonComponent } from './shared/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
